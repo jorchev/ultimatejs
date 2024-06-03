4cff7c4c4c78810af2479b4fd8d0fe216d3ffeb7
Jorge Vega <j.vega@mafasa.org>
2024-06-03 07:55:38 -0700

    Reglas para nombrar variables

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -1,5 +1,11 @@
+// Convenciones para nombrar variables 
+// UpperCamelCase, camelCase, snake_case
 let nombre = "Hola Mundo";
-let apellido;
-
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
 console.log(nombre);
-console.log(apellido);
+console.log(NombreCompleto);
+
+let apellido, animal;
+apellido = 'Vega';
