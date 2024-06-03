8c5fff7bf672096705ddd572c076b4bef7e8e7b0
Jorge Vega <j.vega@mafasa.org>
2024-06-03 09:45:20 -0700

    Tipado dinámico

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,12 @@
+let numero = 42;
+let nombre = "Hola mundo";
+let verdadero = true;
+let undef;
+let nulo = null;
+
+// nombre = 53;
+console.log(typeof numero);
+console.log(typeof nombre);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nulo);
