5f630aaa02a4917a74ef54b9ab04f8e94eb1bd72
Jorge Vega <j.vega@mafasa.org>
2024-06-10 14:14:14 -0700

    Array

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,12 @@
+let animales = ['chancito','caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+
+animales[2] = 'dragón';
+console.log(animales);
+
+animales[10] = 'pez';
+console.log(animales[7]);
+console.log(typeof animales);
+console.log(animales.length);
