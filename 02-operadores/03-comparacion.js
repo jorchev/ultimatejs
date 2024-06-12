b0b2f395b171f943b671e82f53ec1cf952495a20
Jorge Vega <j.vega@mafasa.org>
2024-06-12 08:37:58 -0700

    Operadores de comparación

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,14 @@
+let a = 10;
+
+// Operadores relacionales
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+
+// Operadores de igualdad
+console.log(a == 10);
+console.log(a != 10);
+console.log(a === '10');
+console.log(a !== '10');
