da5e4eefaa110ee1e4c4809a70b00079636a14cd
Jorge Vega <j.vega@mafasa.org>
2024-06-15 10:20:18 -0700

    Ejercicio 05 - solución Nico

--- 04-ejercicios/05.js
+++ 04-ejercicios/05.js
@@ -3,34 +3,45 @@
  * menor y mayor de un array
  */
 
-let array = [2,5,7,300,-150,15,-5,-100,55];
+let array = [2,5,7,300,450,-150,15,-175,-5,-100,55];
 
 function getMenorMayor(arr){
 
-    let numeros = [];
+    // let numeros = [];
 
-    let aux = array[0];
-    for(let numero of array){
-        if(numero < aux){
-            aux = numero;
-        }
-    }
-    numeros.push(aux);
+    // let aux = array[0];
+    // for(let numero of array){
+    //     if(numero < aux){
+    //         aux = numero;
+    //     }
+    // }
+    // numeros.push(aux);
 
-    aux = array[0];
-    for(let numero of array){
-        if(numero > aux){
-            aux = numero;
-        }
+    // aux = array[0];
+    // for(let numero of array){
+    //     if(numero > aux){
+    //         aux = numero;
+    //     }
         
-    }
-    numeros.push(aux);
+    // }
+    // numeros.push(aux);
 
-    return numeros;
+    // return numeros;
+
+
+    // Nicolas utilizo operadores terniarios con dos variables una para menor y otra para mayor
+    let menor = arr[0];
+    let mayor = arr[0];
+
+    for(let numero of arr){
+        menor = numero < menor ? numero : menor;
+        mayor = numero > mayor ? numero : mayor;
+    }
+
+    return [menor, mayor];
 
 }
 
 let numeros = getMenorMayor(array);
 console.log(numeros);
 
-// Nicolas utilizo operadores terniarios con dos variables una para menor y otra para mayor
