fd7cb0acae7476301653149555633bfe50b0b00f
Jorge Vega <jorge.hvr@gmail.com>
2024-06-14 20:01:11 -0700

    Ejercicio 04

---
+++ 04-ejercicios/04.js
@@ -0,0 +1,11 @@
+/**
+ * Números impares del 0 al 10
+ */
+
+for(let i=0;i <= 10;i++){
+    if(i%2===1){
+        console.log('impar',i);
+    }
+}
+
+// Niclolas lo resolvio con un while y como condicion !== 0
