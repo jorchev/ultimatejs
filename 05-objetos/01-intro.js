9d3785aa35d8a9ffe4a8647267a94e0d66bb77c8
Jorge Vega <jorge.hvr@gmail.com>
2024-06-24 16:06:53 -0700

    Introducción a objetos

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,12 @@
+let user = {
+    email: 'jorge.hvr@gmail.com',
+    nombre: 'Jorge',
+    direccion: {
+        calle: 'calle 14',
+        numero: 1808,
+    },
+    activo: true,
+    recuperarClave: function (){
+        console.log("Recupeando clave...");
+    }
+};
