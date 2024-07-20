7d31f1a08f75e271cd522e7f8000393dda196257
Jorge Vega <jorge.hvr@gmail.com>
2024-07-20 10:52:03 -0700

    Constructor functions

---
+++ 05-objetos/04-constructor.js
@@ -0,0 +1,11 @@
+// { id: 1, recuperarClave: function(){} }
+
+function Usuario(){
+    this.id = 1;
+    this.recuperarClave = function() {
+        console.log('Recuperando clave...');
+    }
+}
+
+let usuario = new Usuario();
+console.log(usuario);
