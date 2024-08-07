0510696efee8f81b78b070b7712819e8db9317e2
Jorge Vega <j.vega@mafasa.org>
2024-08-07 08:57:06 -0700

    Caracteres de escape

---
+++ 05-objetos/15-escape.js
@@ -0,0 +1,11 @@
+const saludo = "Hola \\mundo!\n bienvenidos a\t \"ultimate javascript\" :)";
+
+/**
+ * \n nueva linea
+ * \t tabulacion
+ * \' comilla simple
+ * \" comilla doble
+ * \\ backslash
+ */
+
+console.log(saludo);
