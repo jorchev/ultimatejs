f86bb3f9182219b9e85e500dabd4c20c017b65d7
Jorge Vega <jorge.hvr@gmail.com>
2024-08-24 23:02:17 -0700

    Buscar primitivos

---
+++ 07-arrays/03-buscar-primitivos.js
@@ -0,0 +1,10 @@
+const letras = ['a','b',1,'c','d',1];
+
+console.log(letras.indexOf('c'));
+console.log(letras.indexOf(1));
+console.log(letras.lastIndexOf(1));
+
+console.log(letras.indexOf(1) != -1);
+console.log(letras.includes('b'));
+
+console.log(letras.indexOf(1,3));
