c63af03cc66f029d0154e7a55f68e4d62514521b
Jorge Vega <jorge.hvr@gmail.com>
2024-08-28 22:41:47 -0700

    Join

---
+++ 07-arrays/09-join.js
@@ -0,0 +1,8 @@
+let arr1 = ['Nicolas','Chanchito','Felipe'];
+let mensaje = arr1.join(', ');
+console.log(mensaje);
+
+let saludo = "Hola mundo! desde MX";
+let dividido = saludo.split(' ');
+console.log(dividido);
+console.log(dividido.join('-'));
