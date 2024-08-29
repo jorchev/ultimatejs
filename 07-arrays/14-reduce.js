2025bbe0c7b35e55a1b37479861bbe2805ea2d8d
Jorge Vega <j.vega@mafasa.org>
2024-08-29 08:33:11 -0700

    Array reduce

---
+++ 07-arrays/14-reduce.js
@@ -0,0 +1,30 @@
+const numeros = [1, 2, 3, 4];
+
+const suma = numeros.reduce((acc, el) => {
+    return acc + el;
+}, 0);
+
+console.log(suma);
+
+
+
+const anidado = [[1, 2], 3, [4, 5]];
+const plano = anidado.reduce((acc, el) => acc.concat(el), []);
+
+console.log(plano);
+
+
+
+const usuarios = [
+    { edad: 17, nombre: 'Nico' },
+    { edad: 13, nombre: 'Chanchito' },
+    { edad: 25, nombre: 'Felipe' },
+    { edad: 32, nombre: 'Fernanda' },
+];
+
+const indexado = usuarios.reduce((acc, el) => ({
+    ...acc,
+    [el.nombre]: el,
+}), {});
+
+console.log(indexado);
