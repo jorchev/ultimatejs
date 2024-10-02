0e55fd144c75756730a0288f91c92ab2e7102582
Jorge Vega <j.vega@mafasa.org>
2024-10-02 08:30:41 -0700

    4 principios de la POO

--- 11-prototipos/01-principios.js
+++ 11-prototipos/01-principios.js
@@ -10,6 +10,8 @@ let fullname = getNombreCompleto(nombre, apellido);
 
 
 // Ejemplo de programación orientada a objetos
+
+// Primer principio encapsulación
 const user = {
     nombre: 'Hola',
     apellido: 'Mundo',
@@ -19,3 +21,26 @@ const user = {
 }
 
 console.log(user.getNombreCompleto());
+
+
+// Segundo principio Abstracción
+const user1 = new User();
+user1.password = 'Chanchito feliz';
+user1.save();
+
+// Tercer principio Herencia
+/**
+ * User --> id, name, guardar()
+ * Restaurante --> id, name, guardar()
+ * Motociclista --> id, name, guardar()
+ * En JS para solventar este problemas las propiedades y métodos como 
+ * id, name, guardar() se crean en una función constructora y se 
+ * transfieren a los objetos User, Restaurante y Motociclista
+ */
+
+// Cuarto principio Polimorfismo
+function validaEntidad(entidad) {
+    // ...
+
+    entidad.save();
+}
