964da542273caf034d9e312ffab846a2102f7278
Jorge Vega <j.vega@mafasa.org>
2024-10-05 12:51:59 -0700

    Composición VS herencia

---
+++ 12-herencia/05-comp-vs-her.js
@@ -0,0 +1,17 @@
+// vuela, nada, camina, va al baño
+function Pato () {};
+
+// camina, nada, va al baño
+function Perro () {};
+
+// nada, va al baño
+function Pez () {};
+
+// vuela pero no nada, camina, ni va al baño
+function Avion () {};
+
+
+// Notas de la explicación:
+// En este ejemplo sería prosible usar herencia pero sería muy complejo, ya que serian muchos niveles
+// de Prototipos, Nicolas recomienda solo utilizar un nivel de prototipos si se ocupa mas mejor pasar
+// a los mixins
